65a089b04d93cae3f780787a1ac9f6b93c2ae028
balu <balucc12@gmail.com>
2025-02-25 12:31:39 +0530

    updated jenkins file

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -34,7 +34,7 @@ pipeline {
         stage('Quality Gate') {
             steps {
                 script {
-                    waitForQualityGate abortPipeline: false, credentialsId: 'Sonar-token'
+                    waitForQualityGate abortPipeline: True, credentialsId: 'Sonar-token'
                 }
             }
         }
